4eece491fb298f7a431737b8b7c135c38ab12e2c
Ian Withers <mr.bowtie989@gmail.com>
2022-03-09 13:54:52 -0500

    feat: create unique slug generator

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,15 @@
 class Link < ApplicationRecord
+  validates :url, presence: true
+  validates :slug, uniqueness: true
+
+  def generate_slug
+    loop do
+      short_link = rand(32 ** 6).to_s(16)
+      return short_link if Link.where(slug: short_link).empty?
+    end
+  end
+
+  def short_link
+    "localhost:3000/links/#{slug}"
+  end
 end
